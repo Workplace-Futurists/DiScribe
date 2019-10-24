bd4ad4c28472da37076767dbac2cea867fb5aac2
levanayang <levanavyang@gmail.com>
2019-10-23 17:54:11 -0700

    added console app used in the wip link
    Former-commit-id: f6ac5070e8eff004f545213906cc4b7cabe2468a

---
+++ dialer/wip_link_dialer/dialer_01/dialer_01/Dialer/TwilioClientSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Twilio;
+
+namespace dialer_01.Dialer
+{
+    public class TwilioClientSingleton
+    {
+        // the instance variable of this client
+        private static TwilioClientSingleton instance;
+        private string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        private string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        private TwilioClientSingleton() { }
+
+        public static TwilioClientSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TwilioClientSingleton();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
