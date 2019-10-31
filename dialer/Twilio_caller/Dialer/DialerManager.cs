a266f6bfa3c28b56130561b92e77ca74097d41b1
Jin Huang <jinhuang696@gmail.com>
2019-10-31 23:28:07 +0000

    added module folders
    dialer and transcriber

---
+++ dialer/Twilio_caller/Dialer/DialerManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace twilio_caller.dialer
+{
+    public class dialerManager
+    {
+        //// TODO: can't reference class in main program due to static referencing when 
+        //// instantiating call to methods need to fix before this can be encapsuled
+        //private string meetingNum { get; set; }
+
+        //public dialerManager(string mnum)
+        //{
+        //    meetingNum = mnum;
+        //}
+
+        // a function to add pauses ('w' characters) between meeting call in numbers and extensions
+        // ex. 628079791
+        private string formatDigits(string meetingNum)
+        {
+            // TODO: assert length of meeting number 
+
+            // add necessary digits and pauses ('w') for send digits
+            var result = "wwwwwwwwww1ww#wwww" +
+                meetingNum[0] + "w" +
+                meetingNum[1] + "w" +
+                meetingNum[2] + "w" +
+                meetingNum[3] + "w" +
+                meetingNum[4] + "w" +
+                meetingNum[5] + "w" +
+                meetingNum[6] + "w" +
+                meetingNum[7] + "w" +
+                meetingNum[8] + "w#wwwww#";
+
+            // return the result after concat
+            return result;
+        }
+
+        public string CallMeeting(string mNum)
+        {
+            var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+            TwilioClient.Init(accountSid, authToken);
+
+            // From the Twilio SIP guide
+            //var call = CallResource.Create(
+            //    url: new Uri("http://www.example.com/sipdial.xml"),
+            //    to: new PhoneNumber("sip:628353018@cs319-futurists-test01.my.webex.com"),
+            //    from: new PhoneNumber("+17787444195")
+            //);
+
+            // Tried using the SIP of a webex meeting and it fails. probably because this isn't an async method
+            //var to = new PhoneNumber("sip:628353018@cs319-futurists-test01.my.webex.com");
+
+            const string vancouverTollNum = "+12268289662";
+            const string twilioAccNum = "+17787444195";
+            //string meetingNum = "628079791";
+
+            // this is the webex call vancouver toll number
+            var to = new PhoneNumber(vancouverTollNum);
+            // This will work if you call a verified phone number (currently has mine)
+            //var to = new PhoneNumber("+17786886112");
+
+            // This is the twilio number linked to our account
+            var from = new PhoneNumber(twilioAccNum);
+
+            // makes the call resource to send
+            var call = CallResource.Create(to, from,
+                //method: Twilio.Http.HttpMethod.Get,
+                sendDigits: formatDigits(mNum),
+                // Records the outgoing call
+                record: true,
+                // I think this is a default message that plays from the url?
+                //url: new Uri("http://lonelycompany.ca/test.xml")
+                // default demo uri
+                url: new Uri("http://demo.twilio.com/docs/voice.xml")
+             );
+
+            return call.Sid;
+        }
+    }
+}
---
+++ transcriber/TranscribeAgent/SpeechTranscriber.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Intent;
+
+namespace transcriber.TranscribeAgent
+{
+    /// <summary>
+    /// Provides transcription of a set of AudioSegments to create a formatted text file. 
+    /// <para>See <see cref="TranscribeAgent.AudioSegment"></see> documentation for more info on AudioSegment.</para>
+    /// <para>Uses the Microsoft Azure Cognitive Services Speech SDK to perform transcription of audio streams
+    /// within each AudioSegment. </para>
+    /// </summary>
+    class Speechtranscriber
+    {
+        public Speechtranscriber(SortedList<AudioSegment, AudioSegment> audioSegments)
+        {
+            AudioSegments = audioSegments;
+        }
+
+        /// <summary>
+        /// SortedList where the the AudioSegments are sorted by their Offset property. 
+        /// This supports transcription in the correct order.
+        /// </summary>
+        public SortedList<AudioSegment, AudioSegment> AudioSegments { get; set; }
+
+
+        /// <summary>
+        /// Creates an audio transcript text file. The transcript contains speaker names,
+        /// timestamps, and the contents of what each speaker said.
+        /// 
+        /// <para> The transcription follows the the correct order, so that
+        /// the beginning of the meeting is at the start of the file, and the last
+        /// speech around the end of the meeting is at the end of the file.</para>
+        /// </summary>
+        /// <returns>FileInfo object for the transcription output text file.</returns>
+        public FileInfo CreateTranscription()
+        {
+            return new FileInfo("");
+        }
+
+        public static async Task RecognitionWithPullAudioStreamAsync(PullAudioInputStream theStream)
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("c9b69428770c48bc871e23ae97490a63", "centralus");
+
+            var stopRecognition = new TaskCompletionSource<int>();
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"record\minutes.txt", true))
+            {
+                using (var audioInput = AudioConfig.FromStreamInput(theStream))
+                {
+                    // Creates a speech recognizer using audio stream input.
+                    using (var recognizer = new SpeechRecognizer(config, audioInput))
+                    {
+                        // Subscribes to events.
+                        recognizer.Recognizing += (s, e) =>
+                        {
+                            //
+                        };
+                        recognizer.Recognized += (s, e) =>
+                        {
+                            if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                            {
+                                Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                                file.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                            }
+                            else if (e.Result.Reason == ResultReason.NoMatch)
+                            {
+                                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                                file.WriteLine($"NOMATCH: Speech could not be recognized.");
+                            }
+                        };
+
+                        recognizer.Canceled += (s, e) =>
+                        {
+                            Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                            if (e.Reason == CancellationReason.Error)
+                            {
+                                Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                                Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                                Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                            }
+
+                            stopRecognition.TrySetResult(0);
+                        };
+
+                        recognizer.SessionStarted += (s, e) =>
+                        {
+                            Console.WriteLine("\nSession started event.");
+                        };
+
+                        recognizer.SessionStopped += (s, e) =>
+                        {
+                            Console.WriteLine("\nSession stopped event.");
+                            Console.WriteLine("\nStop recognition.");
+                            stopRecognition.TrySetResult(0);
+                        };
+
+                        Console.Write("Awaiting recognition completeion");
+                        // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                        await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                        // Waits for completion.
+                        // Use Task.WaitAny to keep the task rooted.
+                        Task.WaitAny(new[] { stopRecognition.Task });
+
+                        Console.Write("Awaiting recogniotion stop");
+                        // Stops recognition.
+                        await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+    }
+}
