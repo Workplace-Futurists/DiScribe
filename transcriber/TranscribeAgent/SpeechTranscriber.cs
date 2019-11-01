429e3268509a53028258e5af43d746ab8a09eac0
Jin Huang <jinhuang696@gmail.com>
2019-10-31 17:23:02 -0700

    fix conflicts
    Former-commit-id: 7578c0d8f86ac79ab6c7efa7a9d360967d16d661 [formerly 8d29c8ea8b558c9d360ee8a019a943e1964b3873]
    Former-commit-id: c5e77718a7f63d58c792c3abd53934f69c5e9a91

--- transcriber/TranscribeAgent/SpeechTranscriber.cs
+++ transcriber/TranscribeAgent/SpeechTranscriber.cs
@@ -10,40 +10,48 @@ using Microsoft.CognitiveServices.Speech.Intent;
 namespace transcriber.TranscribeAgent
 {
     /// <summary>
-    /// Provides transcription of a set of AudioSegments to create a formatted text file. 
+    /// Provides transcription of a set of AudioSegments to create a formatted text file.
     /// <para>See <see cref="TranscribeAgent.AudioSegment"></see> documentation for more info on AudioSegment.</para>
     /// <para>Uses the Microsoft Azure Cognitive Services Speech SDK to perform transcription of audio streams
     /// within each AudioSegment. </para>
     /// </summary>
     class Speechtranscriber
     {
-        public Speechtranscriber(SortedList<AudioSegment, AudioSegment> audioSegments)
+        public SpeechTranscriber(SortedList<AudioSegment, AudioSegment> audioSegments, FileInfo outFile)
         {
             AudioSegments = audioSegments;
+            MeetingMinutes = outFile;
         }
 
         /// <summary>
-        /// SortedList where the the AudioSegments are sorted by their Offset property. 
+        /// SortedList where the the AudioSegments are sorted by their Offset property.
         /// This supports transcription in the correct order.
         /// </summary>
         public SortedList<AudioSegment, AudioSegment> AudioSegments { get; set; }
+
+        public FileInfo MeetingMinutes { get; set; }
 
 
         /// <summary>
         /// Creates an audio transcript text file. The transcript contains speaker names,
         /// timestamps, and the contents of what each speaker said.
-        /// 
+        ///
         /// <para> The transcription follows the the correct order, so that
         /// the beginning of the meeting is at the start of the file, and the last
         /// speech around the end of the meeting is at the end of the file.</para>
         /// </summary>
         /// <returns>FileInfo object for the transcription output text file.</returns>
-        public FileInfo CreateTranscription()
+        public async void CreateTranscription()
         {
-            return new FileInfo("");
+            FileInfo outFile = new FileInfo(@"record\minutes.txt");
+
+            //foreach (var segment in AudioSegments)
+
+            RecognitionWithPullAudioStreamAsync(AudioSegments[AudioSegments.Keys[0]].AudioStream, outFile).Wait();
+
         }
 
-        public static async Task RecognitionWithPullAudioStreamAsync(PullAudioInputStream theStream)
+        public static async Task RecognitionWithPullAudioStreamAsync(PullAudioInputStream theStream, FileInfo outFile)
         {
             // Creates an instance of a speech config with specified subscription key and service region.
             // Replace with your own subscription key and service region (e.g., "westus").
@@ -52,7 +60,7 @@ namespace transcriber.TranscribeAgent
             var stopRecognition = new TaskCompletionSource<int>();
 
             using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@"record\minutes.txt", true))
+            new System.IO.StreamWriter(outFile.FullName, true))
             {
                 using (var audioInput = AudioConfig.FromStreamInput(theStream))
                 {
