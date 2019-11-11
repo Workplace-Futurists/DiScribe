bb5003fdd44c4bc0c706dcaec7e8be319987a775
levanayang <levanavyang@gmail.com>
2019-11-10 16:54:17 -0800

    added new user&pass authorization method (no more manually signing into email for ms graph)
    Former-commit-id: ff13e057f7d2cc4cf3607139241c7f9eda3ee81c [formerly 673ee68763607e782ee02869af15a809f4254d5a]
    Former-commit-id: 86f6dee5ca5c4e805626e2b90560e14827472346

--- dialer/Twilio_caller/Program.cs
+++ dialer/Twilio_caller/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,124 +17,129 @@ namespace twilio_caller
 {
     class Program
     {
-        // main program
-        static void Main(string[] args)
+        static IConfigurationRoot LoadAppSettings()
         {
-            static IConfigurationRoot LoadAppSettings()
+            var appConfig = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("PrivateSettings.json", false, true)
+                .Build();
+
+            // Check for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+            string.IsNullOrEmpty(appConfig["username"]) ||
+            string.IsNullOrEmpty(appConfig["password"]) ||
+                // Make sure there's at least one value in the scopes array
+                string.IsNullOrEmpty(appConfig["scopes:0"]))
             {
-                var appConfig = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", false, true)
-                    .Build();
-
-                // Check for required settings
-                if (string.IsNullOrEmpty(appConfig["appId"]) ||
-                    // Make sure there's at least one value in the scopes array
-                    string.IsNullOrEmpty(appConfig["scopes:0"]))
-                {
-                    return null;
-                }
-
-                return appConfig;
+                return null;
             }
 
-            static string FormatDateTimeTimeZone(Microsoft.Graph.DateTimeTimeZone value)
+            return appConfig;
+        }
+
+        static string FormatDateTimeTimeZone(Microsoft.Graph.DateTimeTimeZone value)
+        {
+            // Get the timezone specified in the Graph value
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById(value.TimeZone);
+            // Parse the date/time string from Graph into a DateTime
+            var dateTime = DateTime.Parse(value.DateTime);
+
+            // Create a DateTimeOffset in the specific timezone indicated by Graph
+            var dateTimeWithTZ = new DateTimeOffset(dateTime, timeZone.BaseUtcOffset)
+                .ToLocalTime();
+
+            return dateTimeWithTZ.ToString("g");
+        }
+
+        static void ListCalendarEvents()
+        {
+            var events = Graph.GraphHelper.GetEventsAsync().Result;
+
+            Console.WriteLine("Events:");
+
+            foreach (var calendarEvent in events)
             {
-                // Get the timezone specified in the Graph value
-                var timeZone = TimeZoneInfo.FindSystemTimeZoneById(value.TimeZone);
-                // Parse the date/time string from Graph into a DateTime
-                var dateTime = DateTime.Parse(value.DateTime);
-
-                // Create a DateTimeOffset in the specific timezone indicated by Graph
-                var dateTimeWithTZ = new DateTimeOffset(dateTime, timeZone.BaseUtcOffset)
-                    .ToLocalTime();
-
-                return dateTimeWithTZ.ToString("g");
-            }
-
-            static void ListCalendarEvents()
-            {
-                var events = Graph.GraphHelper.GetEventsAsync().Result;
-
-                Console.WriteLine("Events:");
-
-                foreach (var calendarEvent in events)
-                {
-                    Console.WriteLine($"Subject: {calendarEvent.Subject}");
-                    Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
-                    Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start)}");
-                    Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End)}");
-                }
-            }
-
-            static void Main(string[] args)
-            {
-                Console.WriteLine(".NET Core Graph Tutorial\n");
-
-                var appConfig = LoadAppSettings();
-
-                if (appConfig == null)
-                {
-                    Console.WriteLine("Missing or invalid appsettings.json...exiting");
-                    return;
-                }
-
-                var appId = appConfig["appId"];
-                var scopes = appConfig.GetSection("scopes").Get<string[]>();
-
-                // Initialize the auth provider with values from appsettings.json
-                var authProvider = new GraphAuthentication.DeviceCodeAuthProvider(appId, scopes);
-
-                // Request a token to sign in the user
-                var accessToken = authProvider.GetAccessToken().Result;
-
-                // Initialize Graph client
-                Graph.GraphHelper.Initialize(authProvider);
-
-                // Get signed in user
-                var user = Graph.GraphHelper.GetMeAsync().Result;
-                Console.WriteLine($"Welcome {user.DisplayName}!\n");
-
-                int choice = -1;
-
-                while (choice != 0)
-                {
-                    Console.WriteLine("Please choose one of the following options:");
-                    Console.WriteLine("0. Exit");
-                    Console.WriteLine("1. Display access token");
-                    Console.WriteLine("2. List calendar events");
-
-                    try
-                    {
-                        choice = int.Parse(Console.ReadLine());
-                    }
-                    catch (System.FormatException)
-                    {
-                        // Set to invalid value
-                        choice = -1;
-                    }
-
-                    switch (choice)
-                    {
-                        case 0:
-                            // Exit the program
-                            Console.WriteLine("Goodbye...");
-                            break;
-                        case 1:
-                            // Display access token
-                            Console.WriteLine($"Access token: {accessToken}\n");
-                            break;
-                        case 2:
-                            // List the calendar
-                            ListCalendarEvents();
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice! Please try again.");
-                            break;
-                    }
-                }
+                Console.WriteLine($"Subject: {calendarEvent.Subject}");
+                Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
+                Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start)}");
+                Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End)}");
             }
         }
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core Graph Tutorial\n");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid PrivateSettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            string username = appConfig["username"];
+            SecureString password = new SecureString();
+            // add password from privatesettings.json into secure string
+            foreach (char c in appConfig["password"])
+            {
+                password.AppendChar(c);
+            }
+
+            var scopes = appConfig.GetSection("scopes").Get<string[]>();
+
+            // Initialize the auth provider with values from appsettings.json
+            var authProvider = new GraphAuthentication.UserPassAuthProvider(appId, username, password, scopes);
+
+            // Request a token to sign in the user
+            var accessToken = authProvider.GetAccessToken().Result;
+
+            // Initialize Graph client
+            Graph.GraphHelper.Initialize(authProvider);
+
+            // Get signed in user
+            var user = Graph.GraphHelper.GetMeAsync().Result;
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Please choose one of the following options:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Display access token");
+                Console.WriteLine("2. List calendar events");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    // Set to invalid value
+                    choice = -1;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        // Exit the program
+                        Console.WriteLine("Goodbye...");
+                        break;
+                    case 1:
+                        // Display access token
+                        Console.WriteLine($"Access token: {accessToken}\n");
+                        break;
+                    case 2:
+                        // List the calendar
+                        ListCalendarEvents();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
+        }
     }
 }
