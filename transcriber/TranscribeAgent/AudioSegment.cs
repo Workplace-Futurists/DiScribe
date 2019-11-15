8f7fdd425995c55be239f5dc6ec33200615df18a
Oloff <oloff8@hotmail.com>
2019-11-14 17:09:09 -0800

    Initial implementation for speaker recognition

--- transcriber/TranscribeAgent/AudioSegment.cs
+++ transcriber/TranscribeAgent/AudioSegment.cs
@@ -16,6 +16,7 @@ namespace transcriber.TranscribeAgent
             AudioStream = audioStream;
             StartOffset = startOffset;
             EndOffset = endOffset;
+            
         }
 
 
