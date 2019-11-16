9f2c77d069f1f4da6620bb41033fed842615403e
levanayang <levanavyang@gmail.com>
2019-11-16 12:31:01 -0800

    changed authentication flow (into appsettings.json), refactoredemail subscription methods into GraphHelper.cs. For now removed ms function sdk which was causing errors

--- dialer/Twilio_caller/Graph/GraphHelper.cs
+++ dialer/Twilio_caller/Graph/GraphHelper.cs
@@ -2,15 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace twilio_caller.Graph
 {
     public class GraphHelper
     {
-        private static GraphServiceClient graphClient;
+        private static GraphServiceClient _graphClient;
+
+        private static Subscription _mailSubscription;
+        private const int MAX_SUB_EXPIRATION_MINS = 4230;
+
         public static void Initialize(IAuthenticationProvider authProvider)
         {
-            graphClient = new GraphServiceClient(authProvider);
+            _graphClient = new GraphServiceClient(authProvider);
         }
 
         public static async Task<User> GetMeAsync()
@@ -18,7 +24,7 @@ namespace twilio_caller.Graph
             try
             {
                 // GET /me
-                return await graphClient.Me.Request().GetAsync();
+                return await _graphClient.Me.Request().GetAsync();
             }
             catch (ServiceException ex)
             {
@@ -31,7 +37,7 @@ namespace twilio_caller.Graph
             try
             {
                 // GET /me/events
-                var resultPage = await graphClient.Me.Events.Request()
+                var resultPage = await _graphClient.Me.Events.Request()
                     // Only return the fields used by the application
                     .Select("subject,organizer,start,end")
                     // Sort results by when they were created, newest first
@@ -44,6 +50,73 @@ namespace twilio_caller.Graph
             {
                 Console.WriteLine($"Error getting events: {ex.Message}");
                 return null;
+            }
+        }
+
+        public static async Task<Message> GetEmailAsync()
+        {
+            var messages = await _graphClient.Me.Messages
+                .Request()
+                .Select(e => new {
+                    e.Body
+                })
+                .GetAsync();
+            return messages[0];
+        }
+
+        public static async Task<string> GetEmailMeetingNumAsync()
+        {
+            try
+            {
+                // Get message from mailbox
+                Message message = await GetEmailAsync();
+
+                string accessCode;
+                //string meetingStart;
+                //Boolean pm = false;
+                string parsedEmail = message.Body.Content;
+                parsedEmail = WebUtility.HtmlDecode(parsedEmail);
+                HtmlDocument htmldoc = new HtmlDocument();
+                htmldoc.LoadHtml(parsedEmail);
+                //htmldoc.DocumentNode.SelectNodes("//comment()")?.Foreach(c => c.Remove());
+                parsedEmail = htmldoc.DocumentNode.InnerText;
+                accessCode = parsedEmail.Substring(parsedEmail.IndexOf("Meeting number (access code):"), 41);
+                accessCode = accessCode.Substring(accessCode.IndexOf(':') + 2, 11);
+                accessCode = accessCode.Replace(" ", "");
+
+                return accessCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in GetEmailMeetingNumAsync: " + ex.Message);
+                return "000000000";
+            }
+        }
+
+        public static async Task AddMailSubscription(Subscription sub)
+        {
+            try
+            {
+                _mailSubscription = new Subscription
+                {
+                    ChangeType = "created,updated",
+                    NotificationUrl = "https://discribefunctionapp.azurewebsites.net/api/OutlookMessageWebhookCreator1?code=oCrAsapgfgt68ChnQMGBmkTsYOdRuEGT2KB3yogU0ML4rLgdgIWMkQ==",
+                    Resource = "me/mailFolders('Inbox')/messages",
+                    // This is the max expiration datetime for a mail subscription
+                    ExpirationDateTime = DateTime.Now.AddMinutes(MAX_SUB_EXPIRATION_MINS),
+                    ClientState = "secretClientValue"
+                };
+
+                var response = await _graphClient.Subscriptions
+                    .Request()
+                    .AddAsync(sub);
+
+                Console.WriteLine($"Returned response from add subscription: {response}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding subscription: {sub}");
+                Console.WriteLine($"Received error: {ex.Message}");
             }
         }
     }
