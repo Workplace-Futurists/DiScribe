92c8f9ea89979877dab1d8a7b5937b87a742a02e
Jin Huang <jinhuang696@gmail.com>
2019-11-20 02:06:51 -0800

    Scheduler & DatabaseController csproj added!

---
+++ Scheduler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Scheduler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // TODO the email listener
+            // TODO retrieves meeting attendees email lists
+            // TODO check if any emails arent registered
+            // TODO send emails with registration link
+            // TODO store meeting in database
+
+            // TODO check if any meeting is happening soon (in 30minutes)
+            // TODO schedule the meeting
+        }
+    }
+}
