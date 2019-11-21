88c7a20d97b8eea44bb67b355e06ca8c409d6eb0
Jin Huang <jinhuang696@gmail.com>
2019-11-21 00:55:37 -0800

    Scheduler now converted to netcore3.0
    Former-commit-id: 735302f64e67c572e39d5ff902e323399a3870ae [formerly 6c2573f2a140130cdbb718275c84b9730805343e] [formerly f4222310dc3044f411ebec2e2d6096f25056eae3 [formerly 40a30bcab141cddce60507af8d5189e2a8830a33]]
    Former-commit-id: c4c7da7b00f406d293fe27d475e0ff4ec462e001 [formerly 11914d0578d0aa5442baa89ee03a8fcd27978ce3]
    Former-commit-id: 1d778eb522d52697673521bcf5b03915f9877435

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -2,19 +2,11 @@
 
 namespace Scheduler
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // TODO the email listener
-            // TODO retrieves meeting attendees email lists
-            // TODO check if any emails arent registered
-            // TODO send emails with registration link
-            // TODO store meeting in database
-
-            // TODO check if any meeting is happening soon (in 30minutes)
-            // TODO schedule the meeting
         }
     }
 }
