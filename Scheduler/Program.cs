18210ca87d1a3ea22eb1d46bd3490028fc7ad8e9
Jin Huang <jinhuang696@gmail.com>
2019-11-22 02:19:21 -0800

    updated Scheduler vague structure
    Former-commit-id: 76ebe8e3f6f4e60791e096e3dacef34e5245881f [formerly 5e4e62953c2b7b4f19cccb885f5876edf1f43493] [formerly acc0574eb5b341380ce421996b257436c74493f2 [formerly 0498154aacd153c0d32d728eeddb4957563d23f3]]
    Former-commit-id: d5aaf15c67d80f36e42b16455923ed04fe80c879 [formerly 5749d2f2c984f452c504f373063771bed3672c95]
    Former-commit-id: d9b2644b4c97fabf14a62e14cd2c495923e95280

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using EmailController.GraphHelper;
+using System.Threading.Tasks;
 
 namespace Scheduler
 {
@@ -6,7 +8,28 @@ namespace Scheduler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TimeSpan runInterval = new TimeSpan(30);
+            try
+            {
+                while (true)
+                {
+                    // check if theres new emails
+                    if (GraphHelper.ifNewEmail().Result)
+                    {
+                        // TODO get accesscode
+                        var access_code = GraphHelper.GetEmailMeetingNumAsync();
+                        // TODO get attendees email list
+                        // TODO check with database if any of them arent registered
+                        // TODO sends email to unregistered users with registration link
+                        // TODO refers to database, schedules any meeting that is about to happen within 30 minutes
+                    }
+                    Task.Delay(runInterval).Wait();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error trying to get : " + ex.Message);
+            }
         }
     }
 }
