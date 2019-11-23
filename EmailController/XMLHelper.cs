3363d1a2133ef85f5bbe04840bcfe236e0d2bca5
tmdenddl <seungwook.l95@gmail.com>
2019-11-23 01:06:43 -0800

    Retrieving attendee emails successfully

--- EmailController/XMLHelper.cs
+++ EmailController/XMLHelper.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
 using System.Xml;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace EmailController
 {
     public class XMLHelper
     {
-        public static void GetMeetingAttendee(string xmlServer, string accessCode)
+        public static List<EmailAddress> GetMeetingAttendee(string accessCode)
         {
-            string strXMLServer = xmlServer;
+            string strXMLServer = "https://companykm.my.webex.com/WBXService/XMLService";
 
             WebRequest request = WebRequest.Create(strXMLServer);
             // Set the Method property of the request to POST.
@@ -44,10 +48,22 @@ namespace EmailController
             string responseFromServer = reader.ReadToEnd();
             // Display the content.
             Console.WriteLine(responseFromServer);
+
+            List<string> emails = RetrieveEmails(responseFromServer);
+            List<string> names = RetrieveNames(responseFromServer);
+
+            List<EmailAddress> emailAddresses = GetEmails(emails, names);
+            foreach (EmailAddress email in emailAddresses)
+            {
+                Console.WriteLine(email);
+            }
+        
             // Clean up the streams.
             reader.Close();
             dataStream.Close();
             response.Close();
+
+            return emailAddresses;
         }
 
         public static string GenerateXML(string accessCode)
@@ -74,67 +90,52 @@ namespace EmailController
             return strXML;
         }
 
-        /*
-        public static string GenerateExampleXML()
+        public static List<string> RetrieveEmails(string myXML)
         {
-            string strXML = "<?xml version=\"1.0\" encoding=\"UTF - 8\"?>\r\n";
-            // strXML += "<serv:message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n";
-            strXML += "<serv:message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:serv=\"http://www.webex.com/schemas/2002/06/service\" xsi:schemaLocation=\"http://www.webex.com/schemas/2002/06/service http://www.webex.com/schemas/2002/06/service/service.xsd\">\r\n";
-            strXML += "<header>\r\n";
-            strXML += "<securityContext>\r\n";
-            strXML += "<siteName>companykm.my</siteName>\r\n";
-            strXML += "</securityContext>\r\n";
-            strXML += "</header>\r\n";
-            strXML += "<body>\r\n";
-            strXML += "<bodyContent xsi:type=\"java: com.webex.service.binding.ep.GetAPIVersion\">\r\n";
-            strXML += "</bodyContent>\r\n";
-            strXML += "</body>\r\n";
-            strXML += "</serv:message>\r\n";
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(myXML);
 
-            return strXML;
+            XmlNodeList emailNodes = xml.GetElementsByTagName("com:email");
+
+            List<string> emails = new List<string>();
+
+            foreach (XmlNode emailNode in emailNodes)
+            {
+                emails.Add(emailNode.InnerText);
+            }
+
+            return emails;
         }
 
-        public static void PostXMLRequest(string xmlServer)
+        public static List<string> RetrieveNames(string myXML)
         {
-            string strXMLServer = xmlServer;
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(myXML);
 
-            WebRequest request = WebRequest.Create(strXMLServer);
-            // Set the Method property of the request to POST.
-            request.Method = "POST";
-            // Set the ContentType property of the WebRequest.
-            request.ContentType = "application/x-www-form-urlencoded";
+            XmlNodeList nameNodes = xml.GetElementsByTagName("com:name");
 
-            // Create POST data and convert it to a byte array.
-            string strXML = GenerateExampleXML();
+            List<string> names = new List<string>();
 
-            byte[] byteArray = Encoding.UTF8.GetBytes(strXML);
+            foreach (XmlNode nameNode in nameNodes)
+            {
+                names.Add(nameNode.InnerText);
+            }
 
-            // Set the ContentLength property of the WebRequest.
-            request.ContentLength = byteArray.Length;
-
-            // Get the request stream.
-            Stream dataStream = request.GetRequestStream();
-            // Write the data to the request stream.
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            // Close the Stream object.
-            dataStream.Close();
-            // Get the response.
-            WebResponse response = request.GetResponse();
-
-            // Get the stream containing content returned by the server.
-            dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Console.WriteLine(responseFromServer);
-            // Clean up the streams.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            return names;
         }
-        */
+
+        public static List<EmailAddress> GetEmails(List<string> emails, List<string> names)
+        {
+            List<EmailAddress> emailAddresses = new List<EmailAddress>();
+
+            for (int i = 0; i < emails.Count; i++)
+            {
+                emailAddresses.Add(new EmailAddress(emails[i], names[i]));
+            }
+
+            return emailAddresses;
+        }
+
 
     }
 }
