c367f6c9cf3ac546c057fd5d1cad42be99afeb38
tmdenddl <seungwook.l95@gmail.com>
2019-11-24 23:28:19 -0800

    Getting email succesfully upon creation of meeting

--- MeetingControllers/Program.cs
+++ MeetingControllers/Program.cs
@@ -12,11 +12,22 @@ namespace MeetingControllers
             names.Add("Kevin");
             names.Add("Workplace-futurists");
 
-            List<string> emails = new List<string>();
-            names.Add("seungwook.l95@gmail.com");
-            names.Add("workplace-futurists@hotmail.com");
+            foreach (string name in names)
+            {
+                Console.WriteLine("name: " + name);
+            }
+            
 
-            string startDate = "11/26/2019 18:00:00";
+            List<string> emails = new List<string>();
+            emails.Add("seungwook.l95@gmail.com");
+            emails.Add("workplace-futurists@hotmail.com");
+
+            foreach (string email in emails)
+            {
+                Console.WriteLine("email: " + email);
+            }
+
+            string startDate = "11/26/2019 15:00:00";
             string duration = "30";
 
             string accessCode = MeetingController.CreateWebExMeeting(names, emails, startDate, duration);
