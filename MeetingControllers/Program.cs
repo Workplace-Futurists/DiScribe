3a97cf272b99589b50bb5af254da675bda9cbfa5
Jin Huang <jinhuang696@gmail.com>
2019-11-25 01:00:47 -0800

    fixed email controller bug

--- MeetingControllers/Program.cs
+++ MeetingControllers/Program.cs
@@ -8,28 +8,32 @@ namespace MeetingControllers
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
-            names.Add("Workplace-futurists");
+            EmailController.Initialize();
+            EmailController.SendMail(new List<EmailAddress> {
+                new EmailAddress("jinhuang696@gmail.com", "Jin Huang")
+            }, "Hi");
+            //List<string> names = new List<string>();
+            //names.Add("Workplace-futurists");
 
-            foreach (string name in names)
-            {
-                Console.WriteLine("name: " + name);
-            }
-            
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine("name: " + name);
+            //}
 
-            List<string> emails = new List<string>();
-            emails.Add("workplace-futurists@hotmail.com");
 
-            foreach (string email in emails)
-            {
-                Console.WriteLine("email: " + email);
-            }
+            //List<string> emails = new List<string>();
+            //emails.Add("workplace-futurists@hotmail.com");
 
-            string startDate = "11/26/2019 15:00:00";
-            string duration = "30";
+            //foreach (string email in emails)
+            //{
+            //    Console.WriteLine("email: " + email);
+            //}
 
-            string accessCode = MeetingController.CreateWebExMeeting(names, emails, startDate, duration);
-            Console.WriteLine("AccessCode: " + accessCode);
+            //string startDate = "11/26/2019 15:00:00";
+            //string duration = "30";
+
+            //string accessCode = MeetingController.CreateWebExMeeting(names, emails, startDate, duration);
+            //Console.WriteLine("AccessCode: " + accessCode);
         }
     }
 }
