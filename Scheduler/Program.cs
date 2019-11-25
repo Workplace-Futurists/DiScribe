488e89fff77a61e7f239a1994b3411bbf04fc320
Jin Huang <jinhuang696@gmail.com>
2019-11-24 19:49:54 -0800

    Scheduling renamed to Scheduler
    Former-commit-id: 56a1e3a1445905e1dc10a334075353f1a17816b8 [formerly 73257e6ba4eadf18da41016943d98c68e3b47b0e] [formerly a3ec484292d9709e153bb86e4e335c492a5e1410 [formerly f55fd47d8e263ff96362fdefaac7372bfa360f0e]]
    Former-commit-id: de088a0ec01c730deb5748bf61f3985b84060e43 [formerly 3131114e5eaba60c4975016cf18799767ff897bb]
    Former-commit-id: afb7c775c30c2d2376bae6bfd40a864dd033fb69

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -4,20 +4,12 @@ using System.Text;
 
 namespace Scheduler
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-                       
-            TranscribeScheduler.ScheduleTask("000000", DateTime.Now.AddSeconds(5), "DiScribe.exe", "..");
-
-
-
-        }
-
-
-
-
-
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TranscribeScheduler.ScheduleTask("000000", DateTime.Now.AddSeconds(5),
+				"Main.exe", "C:\\CPSC319\\cs319-2019w1-hsbc\\Main\\bin\\Debug\\netcoreapp3.0");
+		}
+	}
 }
