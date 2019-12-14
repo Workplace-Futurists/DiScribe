274dd217df74ec53c2f48cdc766fc7e4e827995a
Jin Huang <jinhuang696@gmail.com>
2019-12-13 23:15:34 -0800

    Create Meeting.cs

---
+++ DatabaseManager/Data/Meeting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace DiScribe.DatabaseManager.Data
+{
+    public class Meeting : DataElement
+    {
+        public Meeting(string MeetingSubject,
+            DateTime StartTime,
+            DateTime EndTime,
+            string MeetingCode)
+        {
+            this.MeetingSubject = MeetingSubject;
+            this.StartTime = StartTime;
+            this.EndTime = EndTime;
+            this.MeetingCode = MeetingCode;
+        }
+
+        public string MeetingSubject { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string MeetingCode { get; set; }
+
+        public override bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(string lookup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
